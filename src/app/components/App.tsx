cecf8d3f8cbf107ca79b67d9b2c8451a418f8020
iMasanari <14dd023@gmail.com>
2019-09-23 23:44:02 +0900

    feat: スキルフィルターを実装

--- src/app/components/App.tsx
+++ src/app/components/App.tsx
@@ -7,6 +7,7 @@ import SearchButton from './actions/SearchButton'
 import Header from './header/Header'
 import Result from './result/Result'
 import Skill from './skill/Skill'
+import SkillFilter from './skill/SkillFilter'
 
 require('./App.css')
 
@@ -17,13 +18,15 @@ const initSkillLogState: ISkill = JSON.parse(localStorage.getItem(STORAGE_KEY)!)
 const App: React.FC = () => {
   const [activeSkill, updateActiveSkill, clearActiveSkill] = useSkill()
   const [skillLog, setSkillLog] = useState(initSkillLogState as ISkill)
+  const [skillFilter, setSkillFilter] = useState('')
   const [result, setResult] = useState(null as IResult | null)
   const skillRef = useRef<HTMLDivElement>(null)
 
   const skillList = useMemo(() => {
-    return allSkillList.slice()
+    return allSkillList
+      .filter((v) => !skillFilter || ~v.name.indexOf(skillFilter) || v.category === skillFilter)
       .sort((a, b) => (skillLog[b.id] || 0) - (skillLog[a.id] || 0))
-  }, [skillLog])
+  }, [skillFilter, skillLog])
 
   // skillLog変更時
   useLayoutEffect(() => {
@@ -39,6 +42,7 @@ const App: React.FC = () => {
       <Header />
       <main className="App-main">
         <div className="App-inputArea">
+          <SkillFilter value={skillFilter} setValue={setSkillFilter} />
           <div className="App-skill" ref={skillRef}>
             <Skill
               skillList={skillList}
