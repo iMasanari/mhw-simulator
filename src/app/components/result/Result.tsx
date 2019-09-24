157e07e1abdafb9e3f3d98b98913b3d88dd82742
iMasanari <14dd023@gmail.com>
2019-09-24 22:29:02 +0900

    chore: レイアウト修正

--- src/app/components/result/Result.tsx
+++ src/app/components/result/Result.tsx
@@ -1,6 +1,8 @@
 import React, { useLayoutEffect, useRef } from 'react'
 import { Result } from '~/app/service/search'
 
+require('./Result.css')
+
 interface Props {
   result: Result
 }
@@ -17,58 +19,64 @@ const Result: React.FC<Props> = ({ result }) => {
   }, [result])
 
   return (
-    <table ref={elRef}>
-      <tbody>
-        <tr>
-          <th>防御力</th>
-          <td>{result.def}</td>
-        </tr>
-        <tr>
-          <th>頭</th>
-          <td>{result.head}</td>
-        </tr>
-        <tr>
-          <th>胴</th>
-          <td>{result.body}</td>
-        </tr>
-        <tr>
-          <th>腕</th>
-          <td>{result.arm}</td>
-        </tr>
-        <tr>
-          <th>腰</th>
-          <td>{result.wst}</td>
-        </tr>
-        <tr>
-          <th>足</th>
-          <td>{result.leg}</td>
-        </tr>
-        <tr>
-          <th>お守り</th>
-          <td>{result.charm}</td>
-        </tr>
-        <tr>
-          <th>装飾品</th>
-          <td>
-            <ul>
-              {result.decos.map(({ name, count }) =>
-                <li key={name}>{name}x{count}</li>
-              )}
-            </ul>
-          </td>
-        </tr>
-        <tr>
-          <th>発動スキル</th>
-          <td>
-            <ul>
-              {result.skills.map(({ name, count }) =>
-                <li key={name}>{name} Lv{count}</li>
-              )}
-            </ul>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+    <div className="Result" ref={elRef}>
+      <table className="Result-table">
+        <tbody>
+          <tr>
+            <th className="Result-th">頭</th>
+            <td>{result.head}</td>
+          </tr>
+          <tr>
+            <th className="Result-th">胴</th>
+            <td>{result.body}</td>
+          </tr>
+          <tr>
+            <th className="Result-th">腕</th>
+            <td>{result.arm}</td>
+          </tr>
+          <tr>
+            <th className="Result-th">腰</th>
+            <td>{result.wst}</td>
+          </tr>
+          <tr>
+            <th className="Result-th">足</th>
+            <td>{result.leg}</td>
+          </tr>
+          <tr>
+            <th className="Result-th">お守り</th>
+            <td>{result.charm}</td>
+          </tr>
+          <tr>
+            <th className="Result-th">装飾品</th>
+            <td>
+              <ul className="Result-ul">
+                {result.decos.map(({ name, count }) =>
+                  <li key={name}>{name}x{count}</li>
+                )}
+              </ul>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <table className="Result-table">
+        <tbody>
+          <tr>
+            <th className="Result-th">防御力</th>
+            <td>{result.def}</td>
+          </tr>
+          <tr>
+            <th className="Result-th">発動スキル</th>
+            <td>
+              <ul className="Result-ul">
+                {result.skills.map(({ name, count }) =>
+                  <li key={name}>{name} Lv{count}</li>
+                )}
+              </ul>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   )
 }
 
