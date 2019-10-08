9da226acdcf8d986b605a29455116e954a822f81
iMasanari <14dd023@gmail.com>
2019-10-08 23:49:00 +0900

    refactor: workerの名称データの依存を修正

--- src/worker/service/calc.ts
+++ src/worker/service/calc.ts
@@ -1,5 +1,3 @@
-import * as armorsData from '~/app/data'
-import skillList from '~/app/data/skill.json'
 import execute, { Condition } from './execute'
 
 export interface Result {
@@ -9,8 +7,8 @@ export interface Result {
   wst: string | undefined
   leg: string | undefined
   charm: string | undefined
-  decos: { name: string, count: number }[]
-  skills: { name: string, count: number }[]
+  decos: { id: string, count: number }[]
+  skills: { id: string, count: number }[]
   def: number
   slot1: number
   slot2: number
@@ -18,13 +16,8 @@ export interface Result {
   slot4: number
 }
 
-const skillHash = skillList.reduce(
-  (acc, v) => (acc[v.id] = v.name, acc),
-  {} as Record<string, string>
-)
-
-const findArmor = <T>(list: string[], obj: Record<string, T>) =>
-  list.map((value) => obj[value]).find(Boolean)
+const findArmor = (list: string[], prefix: string) =>
+  list.find(id => id.startsWith(prefix))
 
 const getSlots = (result: Record<string, number>) => {
   const slot3Over = Math.min(result.y_1, result.y_2, result.y_3)
@@ -43,20 +36,20 @@ export default (condition: Condition, objective: string): Result => {
 
   const list = Object.keys(result).filter(key => result[key])
 
-  const head = findArmor(list, armorsData.head)
-  const body = findArmor(list, armorsData.body)
-  const arm = findArmor(list, armorsData.arm)
-  const wst = findArmor(list, armorsData.wst)
-  const leg = findArmor(list, armorsData.leg)
-  const charm = findArmor(list, armorsData.charm)
+  const head = findArmor(list, 'xh')
+  const body = findArmor(list, 'xb')
+  const arm = findArmor(list, 'xa')
+  const wst = findArmor(list, 'xw')
+  const leg = findArmor(list, 'xl')
+  const charm = findArmor(list, 'xc')
 
   const decos = list
-    .map(value => ({ name: (armorsData.deco as Record<string, string>)[value], count: result[value] }))
-    .filter(({ name }) => name)
+    .filter(id => id.startsWith('xd'))
+    .map(id => ({ id, count: result[id] }))
 
   const skills = list
-    .map(value => ({ name: skillHash[value], count: result[value] }))
-    .filter(({ name }) => name)
+    .filter(id => id.startsWith('ys'))
+    .map(id => ({ id, count: result[id] }))
     .sort((a, b) => b.count - a.count)
 
   const { ydl: def } = result
