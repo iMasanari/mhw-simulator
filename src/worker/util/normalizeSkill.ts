f199355544697059962d3a940a230c207e532aaf
iMasanari <14dd023@gmail.com>
2019-10-10 17:26:58 +0900

    fix #7: スキル不備を修正

--- src/worker/util/normalizeSkill.ts
+++ src/worker/util/normalizeSkill.ts
@@ -6,57 +6,57 @@ export default (skill: Skill) => {
 
   // スリンガー装填数ＵＰ / 銀火竜の真髄
   if (skill.ys4 > 3) {
-    skill.ys88 = Math.max(skill.ys88 || 0, 2)
+    skill.ys94 = Math.max(skill.ys94 || 0, 2)
   }
 
   // 整備 / 炎妃龍の真髄
   if (skill.ys5 > 3) {
-    skill.ys86 = Math.max(skill.ys86 || 0, 3)
+    skill.ys75 = Math.max(skill.ys75 || 0, 3)
   }
 
   // ボマー / 調査団の錬金術
   if (skill.ys12 > 3) {
-    skill.ys64 = Math.max(skill.ys64 || 0, 3)
+    skill.ys80 = Math.max(skill.ys80 || 0, 3)
   }
 
   // 満足感 / 轟竜の真髄
   if (skill.ys13 > 1) {
-    skill.ys74 = Math.max(skill.ys74 || 0, 3)
-  }
-
-  // 渾身 / 斬竜の真髄
-  if (skill.ys103 > 3) {
-    skill.ys72 = Math.max(skill.ys72 || 0, 3)
-  }
-
-  // 力の解放 / 雷狼竜の真髄
-  if (skill.ys114 > 5) {
-    skill.ys78 = Math.max(skill.ys78 || 0, 3)
-  }
-
-  // 挑戦者 / 砕竜の真髄
-  if (skill.ys116 > 5) {
-    skill.ys73 = Math.max(skill.ys73 || 0, 3)
-  }
-
-  // 砲術 / 熔山龍の真髄
-  if (skill.ys126 > 3) {
     skill.ys85 = Math.max(skill.ys85 || 0, 3)
   }
 
+  // 渾身 / 斬竜の真髄
+  if (skill.ys109 > 3) {
+    skill.ys83 = Math.max(skill.ys83 || 0, 3)
+  }
+
+  // 力の解放 / 雷狼竜の真髄
+  if (skill.ys120 > 5) {
+    skill.ys88 = Math.max(skill.ys88 || 0, 3)
+  }
+
+  // 挑戦者 / 砕竜の真髄
+  if (skill.ys122 > 5) {
+    skill.ys84 = Math.max(skill.ys84 || 0, 3)
+  }
+
+  // 砲術 / 熔山龍の真髄
+  if (skill.ys132 > 3) {
+    skill.ys52 = Math.max(skill.ys52 || 0, 3)
+  }
+
   // 精霊の加護 / 金火竜の真髄
-  if (skill.ys138 > 3) {
-    skill.ys87 = Math.max(skill.ys87 || 0, 2)
+  if (skill.ys144 > 3) {
+    skill.ys93 = Math.max(skill.ys93 || 0, 2)
   }
 
   // ＫＯ術 / 角竜の覇気
-  if (skill.ys144 > 3) {
-    skill.ys67 = Math.max(skill.ys67 || 0, 3)
+  if (skill.ys150 > 3) {
+    skill.ys50 = Math.max(skill.ys50 || 0, 3)
   }
 
   // スタミナ奪取 / 恐暴竜の真髄
-  if (skill.ys146 > 3) {
-    skill.ys77 = Math.max(skill.ys77 || 0, 3)
+  if (skill.ys152 > 3) {
+    skill.ys87 = Math.max(skill.ys87 || 0, 3)
   }
 
   return skill
