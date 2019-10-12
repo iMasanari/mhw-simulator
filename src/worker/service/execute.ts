f8f65045ba7e28faf20fb4f70dfd6a47112d6f67
iMasanari <14dd023@gmail.com>
2019-10-12 21:57:17 +0900

    feat: 複数検索仮実装 (10件検索)
    refs #3

--- src/worker/service/execute.ts
+++ src/worker/service/execute.ts
@@ -11,11 +11,12 @@ export interface Condition {
   armors: Armors
   decos: Decos
   weaponSlots: WeaponSlots
+  prev: string[][]
 }
 
 const createSubject = (data: Record<string, number>, type: number) =>
   Object.entries(data).map(([name, value]) =>
-    ({ name, vars: [{ name, coef: 1 }], bnds: { type, ub: value, lb: value } })
+    ({ vars: [{ name, coef: 1 }], bnds: { type, ub: value, lb: value } })
   )
 
 export default (objectiveName: string, condition: Condition) => {
@@ -25,12 +26,18 @@ export default (objectiveName: string, condition: Condition) => {
     .map(slot => [`cs${slot}`, condition.weaponSlots.filter(v => v >= slot).length] as const)
     .reduce((acc, [key, value]) => (acc[key] = value, acc), {} as Record<string, number>)
 
+  const prevs = condition.prev.map((equips) => ({
+    vars: equips.map(name => ({ name, coef: 1 })),
+    bnds: { type: GLP_UP, ub: equips.length - 1, lb: equips.length - 1 },
+  }))
+
   const subjectTo = [
     ...baseLp.subjectTo,
     ...createSubject(skill, GLP_LO),
     ...createSubject(condition.armors, GLP_FX),
     ...createSubject(condition.decos, GLP_UP),
     ...createSubject(slots, GLP_FX),
+    ...prevs,
   ]
 
   const objective = {
