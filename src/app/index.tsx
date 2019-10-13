b57371bded7dba511b98f5d1cce894c95ac591f3
iMasanari <14dd023@gmail.com>
2019-10-13 16:50:34 +0900

    chore: reduxの準備

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,16 @@
 import 'normalize.css'
 import React from 'react'
 import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
 import App from './components/App'
+import modules from './modules'
+
+const store = createStore(modules)
 
 render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 )
