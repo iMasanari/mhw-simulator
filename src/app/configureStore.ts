89aeca0a444c5e50adddd342ba3a24426d2e67ce
iMasanari <14dd023@gmail.com>
2019-10-31 19:57:28 +0900

    feat: 旧ストアからのマイグレーションを実装

--- src/app/configureStore.ts
+++ src/app/configureStore.ts
@@ -1,7 +1,55 @@
 import { createStore } from 'redux'
-import { PersistConfig, persistReducer, persistStore } from 'redux-persist'
+import { createMigrate, MigrationManifest, PersistConfig, PersistedState, persistReducer, persistStore } from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
 import modules, { RootState } from './modules'
+import { flat } from './util/array'
+import { arm, body, charm, deco, head, leg, skillList, wst } from './util/generatedUtil'
+
+const fromEntries = <T>(list: [string, T][]) =>
+  list.reduce((acc, [k, v]) => (acc[k] = v, acc), {} as Record<string, T>)
+
+const migrateState0 = (state: RootState & PersistedState): RootState & PersistedState => {
+  const skillMap = new Map(skillList.map((v, i) => [`ys${i}`, v.name]))
+
+  const armorMap = new Map(flat(
+    Object.entries({ head, body, arm, wst, leg, charm }).map(([type, v]) =>
+      Object.keys(v).map((name, i) => [`x${type[0]}${i}`, name])
+    )
+  ))
+
+  const decoMap = new Map(
+    Object.keys(deco).map((name, i) => [`xd${i}`, name])
+  )
+
+  const skillLog = fromEntries(
+    Object.entries(state.skillLog)
+      .filter(([id]) => skillMap.has(id))
+      .map(([id, value]) => [skillMap.get(id)!, value])
+  )
+
+  const ignoreArmors = fromEntries(
+    Object.entries(state.ignoreArmors)
+      .filter(([id]) => armorMap.has(id))
+      .map(([id, value]) => [armorMap.get(id)!, value])
+  )
+
+  const decos = fromEntries(
+    Object.entries(state.decos)
+      .filter(([id]) => decoMap.has(id))
+      .map(([id, value]) => [decoMap.get(id)!, value])
+  )
+
+  return {
+    ...state,
+    skillLog,
+    ignoreArmors,
+    decos,
+  }
+}
+
+const migrations: MigrationManifest = {
+  0: (state) => migrateState0(state as RootState & PersistedState),
+}
 
 const whitelist: (keyof RootState)[] = ['decos', 'ignoreArmors', 'skillLog']
 
@@ -9,6 +57,8 @@ const persistConfig: PersistConfig<RootState> = {
   key: 'mhw-simulator',
   storage,
   whitelist,
+  version: 0,
+  migrate: createMigrate(migrations),
 }
 
 const persistedReducer = persistReducer(persistConfig, modules)
