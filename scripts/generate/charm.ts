ed69f49576853c51954d905e69f42d8dd1b2a78d
iMasanari <14dd023@gmail.com>
2019-11-10 08:06:34 +0900

    chore: フォルダ整理

--- scripts/generate/charm.ts
+++ scripts/generate/charm.ts
@@ -2,7 +2,7 @@ import { readCsv } from '../util/fileUtil'
 import fromEntries from '../util/fromEntries'
 
 export const getCharm = async (skillIndexMap: Map<string, number>) => {
-  const csv = await readCsv('./lib/charm.csv')
+  const csv = await readCsv('./lib/fetched/charm.csv')
 
   const charm = csv.map((row, i) => {
     const [名前, レア度, 入手時期, スキル系統1, スキル値1, スキル系統2, スキル値2, 生産素材A1, 個数A1, 生産素材A2, 個数A2, 生産素材A3, 個数A3, 生産素材A4, 個数A4, 前段階, 仮番号] = row
--- scripts/generate/deco.ts
+++ scripts/generate/deco.ts
@@ -2,7 +2,7 @@ import { readCsv } from '../util/fileUtil'
 import fromEntries from '../util/fromEntries'
 
 export const getDeco = async (skillIndexMap: Map<string, number>) => {
-  const csv = await readCsv('./lib/deco.csv')
+  const csv = await readCsv('./lib/fetched/deco.csv')
 
   const result = csv.map((row, i) => {
     const [名前, レア度, スロットサイズ, 入手時期, スキル系統1, スキル値1, スキル系統2, スキル値2, 調査ポイント, 錬金ポイント, 仮番号] = row
