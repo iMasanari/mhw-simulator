d48e9f06973e988c75d06f74e1905721ec847f7a
iMasanari <14dd023@gmail.com>
2019-11-10 10:23:59 +0900

    change: 防具設定をレイアウト変更

--- src/app/components/armors/Armors.tsx
+++ src/app/components/armors/Armors.tsx
@@ -1,29 +1,25 @@
 import React, { useMemo, useState } from 'react'
 import { useIgnoreArmors, useIgnoreArmorsActions } from '~/app/hooks/ignoreArmors'
-import { arm, body, head, leg, wst } from '~/app/util/generatedUtil'
-import ArmorList from './ArmorList'
+import armorGroup from '~/generated/armorGroup.json'
+import ArmorTable from './ArmorTable'
 
 require('./Armors.css')
 
 interface Props {
 }
 
-const createList = (armorData: Record<string, unknown>, filter: string) =>
-  Object.keys(armorData).filter(name => ~name.indexOf(filter))
+const armorGroupEntries = Object.entries(armorGroup)
 
 const Armors: React.FC<Props> = () => {
   const ignoreArmors = useIgnoreArmors()
   const { toggle } = useIgnoreArmorsActions()
-
   const [filter, setFilter] = useState('')
 
-  const deps = [filter]
-
-  const headList = useMemo(() => createList(head, filter), deps)
-  const bodyList = useMemo(() => createList(body, filter), deps)
-  const armList = useMemo(() => createList(arm, filter), deps)
-  const wstList = useMemo(() => createList(wst, filter), deps)
-  const legList = useMemo(() => createList(leg, filter), deps)
+  const list = useMemo(() => (
+    armorGroupEntries
+      .map(([group, equips]) => [group, equips.map(name => (name && ~name.indexOf(filter)) ? name : null)] as const)
+      .filter(([group, equips]) => equips.find(Boolean))
+  ), [filter])
 
   return (
     <div>
@@ -31,19 +27,13 @@ const Armors: React.FC<Props> = () => {
         検索で防具を除外する場合、下記のチェックを外してください。
         <br />
         内容は自動的に保存されますが、開発の都合でリセットされることがあります。
-        </p>
+      </p>
       <input type="text"
         value={filter}
         onChange={e => { setFilter(e.currentTarget.value) }}
         placeholder="フィルタ: 防具名"
       />
-      <div className="Armors-contents">
-        <ArmorList armors={headList} ignoreArmors={ignoreArmors} toggleIgnoreArmors={toggle} />
-        <ArmorList armors={bodyList} ignoreArmors={ignoreArmors} toggleIgnoreArmors={toggle} />
-        <ArmorList armors={armList} ignoreArmors={ignoreArmors} toggleIgnoreArmors={toggle} />
-        <ArmorList armors={wstList} ignoreArmors={ignoreArmors} toggleIgnoreArmors={toggle} />
-        <ArmorList armors={legList} ignoreArmors={ignoreArmors} toggleIgnoreArmors={toggle} />
-      </div>
+      <ArmorTable armorGroups={list} ignoreArmors={ignoreArmors} toggleIgnoreArmors={toggle} />
     </div>
   )
 }
